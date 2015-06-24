c1f6493faeca9a7da618133a3095438dd515deff
Enrique Paniego <epsbgt@gmail.com>
2015-06-25 00:21:17 +0200

    Editar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -75,3 +75,25 @@ exports.create = function(req, res) {
 		}
 	});
 };
+
+
+// GET /quizes/:quizId/edit
+exports.edit = function(req, res) {
+	res.render('quizes/edit', { quiz: req.quiz, errors: [] });
+};
+
+// PUT /quizes/:idQuiz
+exports.update = function(req, res) {
+	req.quiz.pregunta  = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	// guarda en BD los campos pregunta y respuesta de quiz
+	req.quiz.validate().then( function(err) {
+		if (err) {
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors });
+		} else {
+			req.quiz.save( {fields: ["pregunta", "respuesta"]} ).then( function() {
+				res.redirect('/quizes') })
+		}
+	});
+};
